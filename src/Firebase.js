5edc7e0cb30c2f609c6df660a10f65001aef3c1c
github username <patcarrasco@outlook.com>
2019-03-30 14:56:03 -0400

    refactored firebase cred w/ closure

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,9 +1,10 @@
 import * as firebase from 'firebase'
 
-const ROOT_URL = process.env.REACT_APP_ROOT_URL
-
-fetch(ROOT_URL+'/firebase').then(res => res.json()).then(dat => {
-    firebase.initializeApp(dat.key)
-})
+(() => {
+    const url = process.env.REACT_APP_ROOT_URL
+    return fetch(url + '/firebase')
+        .then(res => res.json())
+        .then(dat => firebase.initializeApp(dat.key))
+})()
 
 export default firebase
