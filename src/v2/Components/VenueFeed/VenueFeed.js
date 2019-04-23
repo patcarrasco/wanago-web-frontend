caa7d7823cf20ffd3c129cef673ff278691950a0
github username <patcarrasco@outlook.com>
2019-04-23 02:28:31 -0400

    fixed eventmap info

--- src/v2/Components/VenueFeed/VenueFeed.js
+++ src/v2/Components/VenueFeed/VenueFeed.js
@@ -182,7 +182,7 @@ class VenueFeed extends PureComponent {
                 </Segment>
             </Responsive>
             <Responsive maxWidth={479}>
-               <Segment style={{maxHeight:"26vh", minHeight: "26vh", minWidth:'-webkit-fill-available', maxWidth:'-webkit-fill-available', overflow:'auto', position:'fixed', borderRadius:'unset', marginLeft:'14px', marginRight:'14px'}}>
+               <Segment style={{maxHeight:"26vh", minHeight: "26vh", minWidth:'-webkit-fill-available', overflow:'auto', position:'fixed', borderRadius:'unset', marginLeft:'14px', marginRight:'14px'}}>
                     <VenueInfo closeVenueInfoHandler={this.closeVenueFromMapHandler} />
                 </Segment>
             </Responsive>
